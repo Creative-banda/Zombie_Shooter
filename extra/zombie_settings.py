357fa2b68ef7455c82b0bd886b2c2affe48a35c6
Creative-banda <4kpuspa@gmail.com>
2025-05-08 12:50:28 +0530

    Refactor gun_info dictionary for improved readability

--- extra/zombie_settings.py
+++ extra/zombie_settings.py
@@ -94,31 +94,22 @@ background_music = pygame.mixer.Sound(f"{SOUNDS_DIR / 'background_music.mp3'}")
 # Player Gun Info
 gun_info = {
     "handgun": {
-        "damage": 20,
-        "ammo": 15,
-        "magazine": 6,
-        "cooldown": 0,
-        "remaining_ammo": 6,
-        "sound": SOUNDS_DIR / 'gun_sound/handgun.mp3',
+        "damage": 20, "ammo": 15,
+        "magazine": 6, "cooldown": 0,
+        "remaining_ammo": 6, "sound": SOUNDS_DIR / 'gun_sound/handgun.mp3',
         "reloading_sound" : SOUNDS_DIR / 'gun_sound/handgun_reload.mp3'
     },
     "rifle": {
-        "damage": 50,
-        "ammo": 40,
-        "magazine": 20,
-        "cooldown": 100,
-        "remaining_ammo": 20,
-        "sound": SOUNDS_DIR / 'gun_sound/rifle.mp3',
+        "damage": 50, "ammo": 40,
+        "magazine": 20, "cooldown": 100,
+        "remaining_ammo": 20, "sound": SOUNDS_DIR / 'gun_sound/rifle.mp3',
         "reloading_sound" : SOUNDS_DIR / 'gun_sound/rifle_reload.mp3'
 
     },
     "shotgun": {
-        "damage": 100,
-        "ammo": 10,
-        "magazine": 2,
-        "cooldown": 1000,
-        "remaining_ammo": 2,
-        "sound": SOUNDS_DIR / 'gun_sound/shotgun_shot.mp3',
+        "damage": 100, "ammo": 10,
+        "magazine": 2, "cooldown": 1000,
+        "remaining_ammo": 2, "sound": SOUNDS_DIR / 'gun_sound/shotgun_shot.mp3',
         "reloading_sound" : SOUNDS_DIR / 'gun_sound/shotgun_reload.mp3'
 
     }
